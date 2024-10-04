d96d3d78517f47516692aa74c3c4f1960eb62fdb
d1knight <mdo255da@gmail.com>
2024-10-04 22:28:32 +0500

    full

--- Library/bookings/urls.py
+++ Library/bookings/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('bookings/', views.search_bookings, name = 'search_bookings'),
-    path('bookings/about/<slug:slug>', views.about, name='booking_page')
+    path('bookings/about/<slug:slug>', views.about, name='booking_page'),
+    path('bookings/list/', views.booking_list, name='booking_list'),
+    path('bookings/list/create/', views.booking_create, name='booking_create'),
+    path('bookings/list/update/<int:pk>', views.booking_update, name='booking_update'),
+    path('bookings/list/delete/<int:pk>', views.booking_delete, name='booking_delete'),
 ]
--- Library/bookings/views.py
+++ Library/bookings/views.py
@@ -1,11 +1,111 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404,reverse
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from .models import Booking
+from personal.models import Personal
+from readers.models import Readers
+from Books.models import Books
 from django.template import loader
 from django.core.paginator import Paginator
 from django.db.models import Q
+from django.utils.text import slugify
+from django.urls import reverse
 
+def booking_list(request):
+    booking = Booking.objects.all()
+    return render(request,'bookings/booking_list.html',{'booking':booking})
+
+def booking_create(request):
+    if request.method == 'POST':
+        personal_id = request.POST.get('personal')
+        readers_id = request.POST.get('readers')
+        books_id = request.POST.get('books')
+        get_date = request.POST.get('getDate')
+        return_date = request.POST.get('returnDate')
+
+        # Получение объектов из базы данных по выбранным ID
+        personal = Personal.objects.get(id=personal_id)
+        readers = Readers.objects.get(id=readers_id)
+        books = Books.objects.get(id=books_id)
+
+        # Создание нового заказа
+        booking = Booking.objects.create(
+            personal=personal,
+            readers=readers,
+            books=books,
+            getDate=get_date,
+            returnDate=return_date,
+            slug=slugify(f'{readers.name}-{books.name}-{get_date}')
+        )
+        
+        # После успешного создания заказа перенаправляем на список заказов
+        return redirect(reverse('booking_list'))
+
+    # Если запрос GET — отображаем форму
+    else:
+        personal = Personal.objects.all()
+        readers = Readers.objects.all()
+        books = Books.objects.all()
+
+        context = {
+            'personal': personal,
+            'readers': readers,
+            'books': books,
+        }
+        
+        return render(request, 'bookings/booking_create.html', context)
+    
+
+def booking_update(request, pk):
+    # Получаем существующее бронирование по ID
+    booking = get_object_or_404(Booking, pk=pk)
+    
+    if request.method == 'POST':
+        personal_id = request.POST.get('personal')
+        readers_id = request.POST.get('readers')
+        books_id = request.POST.get('books')
+        get_date = request.POST.get('getDate')
+        return_date = request.POST.get('returnDate')
+
+        # Обновляем объекты в базе данных по выбранным ID
+        booking.personal = Personal.objects.get(id=personal_id)
+        booking.readers = Readers.objects.get(id=readers_id)
+        booking.books = Books.objects.get(id=books_id)
+        booking.getDate = get_date
+        booking.returnDate = return_date
+        booking.slug = slugify(f'{booking.readers.name}-{booking.books.name}-{get_date}')
+
+        # Сохраняем изменения
+        booking.save()
+
+        # Перенаправление на список бронирований
+        return redirect(reverse('booking_list'))
+
+    else:
+        personal = Personal.objects.all()
+        readers = Readers.objects.all()
+        books = Books.objects.all()
+
+        context = {
+            'booking': booking,
+            'personal': personal,
+            'readers': readers,
+            'books': books,
+        }
+        
+        return render(request, 'bookings/booking_update.html', context)
+
+def booking_delete(request, pk):
+    # Получаем бронирование по ID
+    booking = get_object_or_404(Booking, pk=pk)
+    
+    if request.method == 'POST':
+        # Удаляем бронирование
+        booking.delete()
+        # Перенаправляем на список бронирований
+        return redirect(reverse('booking_list'))
+
+    return render(request, 'bookings/booking_confirm_delete.html', {'booking': booking})
 
 
 def search_bookings(request):
